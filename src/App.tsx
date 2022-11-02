4bf96248eb23f57f69c0d6531492082bbc9311f7
stevanovicmilan839 <stevanovicmilan839@gmail.com>
2022-11-02 09:33:54 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
    return(
-     <ThemeProvider>
+     <ThemeProvider theme={defaultTheme}>
       <h1>Hello!</h1>
      </ThemeProvider>
    )
